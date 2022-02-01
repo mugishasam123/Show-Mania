ac89b49a058312e7d3a1e6043741b741be2511da
mugisha Samuel <mugishasamuel42@gmail.com>
2022-02-01 05:19:32 -0800

    Add coment popup

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,9 @@
 import '../css/index.css';
+import Popup from './modules/popup';
+const closeBtn = document.querySelector('.fa')
+const popWindow = document.querySelector('.popup-section');
+
+const pop = new Popup();
+pop.popupDetails(1);
+
+
